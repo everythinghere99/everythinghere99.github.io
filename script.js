24b545ef0f8b03f2cf9bf2ccb2b780d134066216
everythinghere99 <pikapipikachuu2009@gmail.com>
2026-09-22 20:51:08 +0530

    Add SHREE-P184 to affiliateProducts

--- script.js
+++ script.js
@@ -2099,6 +2099,16 @@ const affiliateProducts = [
         description: "Elegant Women's Necklace that adds a simple and stylish touch to your everyday look.",
         price: "₹254",
         affiliateLink: "https://www.amazon.in/dp/B0FB47SJMB?tag=pikafinds-21"
+    },
+    {
+        id: "SHREE-P184",
+        name: "Women's Necklace",
+        images: [
+            "https://m.media-amazon.com/images/I/61ezXExRqgL._SY395_.jpg"
+        ],
+        description: "Elegant Women's Necklace that adds a simple and stylish touch to your everyday look.",
+        price: "₹562",
+        affiliateLink: "https://www.amazon.in/dp/B0C4H4MYNK?tag=pikafinds-21"
     }
 ];
    
