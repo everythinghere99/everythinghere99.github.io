cf53962e418b2ba2d46194a415b6e70f92888c04
everythinghere99 <pikapipikachuu2009@gmail.com>
2026-09-22 21:18:55 +0530

    Add SHREE-P197 to affiliateProducts

--- script.js
+++ script.js
@@ -2229,6 +2229,16 @@ const affiliateProducts = [
         description: "Useful Women's Saree with a stylish design, made for convenient everyday use.",
         price: "₹849",
         affiliateLink: "https://www.amazon.in/dp/B0H42MD89F?tag=pikafinds-21"
+    },
+    {
+        id: "SHREE-P197",
+        name: "Women's Saree",
+        images: [
+            "https://m.media-amazon.com/images/I/6182KJZBz8L._SX342_.jpg"
+        ],
+        description: "Useful Women's Saree with a stylish design, made for convenient everyday use.",
+        price: "₹379",
+        affiliateLink: "https://www.amazon.in/dp/B0CWLHP7HV?tag=pikafinds-21"
     }
 ];
    
